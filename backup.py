71b5454b1a869ccefcfa579f7a977fedc7ada1f8
Jeremy Elson <jelson@gmail.com>
2023-08-06 21:16:00 -0700

    get backup script running

--- backup.py
+++ backup.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
 
+import argparse
 import datetime
 import subprocess
 import sys
 import time
+import yaml
 
 def say(s):
     print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] BACKSTATUS: {s}")
     sys.stdout.flush()
 
-def run(config, cmdline):
-    say(f"running: {cmdline}")
-    subprocess.run(
-        cmdline,
-        stdout=sys.stdout,
-        stderr=sys.stdout,
-        env={
-            'BORG_PASSPHRASE': config['backup-password'],
-        },
-    )
+def run(args, config, cmdline):
+    prefix = "would run" if args.dry_run else "running"
+    say(f"{prefix}: {cmdline}")
 
-def backup_one(config, backupspec):
+    if not args.dry_run:
+        subprocess.run(
+            cmdline,
+            stdout=sys.stdout,
+            stderr=sys.stdout,
+            env={
+                'BORG_PASSPHRASE': config['backup-password'],
+            },
+        )
+
+def backup_one(args, config, backupspec):
+    now = datetime.datetime.now()
     remote_repo =  f"{config['backup-host']}:{backupspec['remote-repo']}"
+    archive_name = f"{remote_repo}::{now.strftime(config['archive-name-format'])}"
 
     say("\n\n")
-    say(f"Starting backup to: {remote_repo}")
+    say(f"Starting backup to: {archive_name}")
 
     # Create a new backup
     cmdline = [
@@ -37,11 +44,11 @@ def backup_one(config, backupspec):
     for exclude in backupspec.get('exclude', []):
         cmdline += ["--exclude", exclude]
     cmdline += [
-        f"{remote_repo}::{config['date']}",
+        archive_name,
     ]
     cmdline += backupspec['local-dirs']
 
-    run(config, cmdline)
+    run(args, config, cmdline)
 
     # Prune old backups
     cmdline = [
@@ -54,7 +61,7 @@ def backup_one(config, backupspec):
         "--keep-weekly", "90",
         "--keep-monthly", "1000",
     ]
-    run(config, cmdline)
+    run(args, config, cmdline)
 
     # List all backups
     cmdline = [
@@ -62,9 +69,9 @@ def backup_one(config, backupspec):
         "list",
         remote_repo,
     ]
-    run(config, cmdline)
+    run(args, config, cmdline)
 
-def backup(config):
+def backup(args, config):
     say("Starting up")
     now = datetime.datetime.now()
     is_workday = now.hour >= 7 and now.hour <= 23
@@ -74,13 +81,37 @@ def backup(config):
         if is_workday and backupspec.get('is-slow', False):
             say(f"Skipping mid-day backup of {backupspec['remote-repo']}")
             continue
-        backup_one(config, backupspec)
+        backup_one(args, config, backupspec)
 
     say("Finishing\n\n\n")
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--dry-run',
+        action='store_true',
+        default=False,
+        help="Don't actually back up",
+    )
+    parser.add_argument(
+        '-c', '--config',
+        required=True,
+        type=str,
+        help='Backup config file (YAML)',
+    )
+    return parser.parse_args()
+
 def main():
-    sys.stdout = open(BACKUP_CONFIG['logfile'], 'a')
-    backup(BACKUP_CONFIG)
+    args = get_args()
+
+    with open(args.config, 'r') as f:
+        config = yaml.safe_load(f)
+
+    if not args.dry_run:
+        sys.stdout = open(config['logfile'], 'a')
+        sys.stderr = sys.stdout
+
+    backup(args, config)
 
 if __name__ == "__main__":
     main()
